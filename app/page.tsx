181dbdd3c6e69453a72ff313c16338c8ebab2d98
Derry Birkett <dbirkett@cloudbees.com>
2026-04-27 18:13:45 +0200

    Make homepage dynamic to fetch articles from Blob storage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { getAllArticles } from "@/lib/content";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const articles = await getAllArticles();
 
